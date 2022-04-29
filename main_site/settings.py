691d7e49d47ce1a7845d3a1adc43c66a2e4e0069
Aliaksiej <alleosin@gmail.com>
2022-04-29 23:54:46 +0300

    Added viecza.herokuapp.com to allowed hosts

--- main_site/settings.py
+++ main_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f7xbi4!_bmda!!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['viecza.herokuapp.com','127.0.0.1']
 
 
 # Application definition
